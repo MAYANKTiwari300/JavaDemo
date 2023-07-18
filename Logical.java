174d1ff0b947f33b105288356bf8fef0a9e0a2de
Mayank Tiwari <127175369+MAYANKTiwari300@users.noreply.github.com>
2023-07-18 19:21:04 +0530

    commit"for_understandingOfLogical_operator"

---
+++ Logical.java
@@ -0,0 +1,26 @@
+
+public class Logical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		        System.out.println("Mayank Tiwari");
+		        int a=10;
+		        int b=7;
+		        int c=20;
+		        int d=15;
+		        
+		        // combination of relational and logical operator
+
+		        System.out.println(a<c);
+		        System.out.println(d<b);
+		        System.out.println(!(a<=b&&b>c&&d<c));
+		        boolean result=(a==b||c>d||d>=b);
+		        System.out.println(result);
+		 
+		
+
+	}
+
+}
