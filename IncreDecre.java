9de43f9f6a9eb7657ccc17c178dd2ff0b2298ff1
Mayank Tiwari <127175369+MAYANKTiwari300@users.noreply.github.com>
2023-07-18 19:22:39 +0530

    Commit"for_Incre_decre_Operator"

---
+++ IncreDecre.java
@@ -0,0 +1,25 @@
+
+public class IncreDecre {
+
+	public static void main(String[] args) {
+	//	int a=5;
+		//int b=a++;
+		
+//		System.out.println(a);
+//		System.out.println(b);
+
+		int a=5;
+		int b=++a;
+		
+	System.out.println(a);
+	System.out.println(b);
+
+		//question2
+		
+		int c=a++ + ++a + --a + a++ + --a;
+		System.out.println(a);
+		System.out.println(c);
+
+	}
+
+}
