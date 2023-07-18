4d75effd3b1276497e351b7d7f72a674e9c4c144
Mayank Tiwari <127175369+MAYANKTiwari300@users.noreply.github.com>
2023-07-18 19:19:26 +0530

    commit for"arithmetic"

---
+++ arith_program.java
@@ -0,0 +1,30 @@
+
+public class arith_program {
+
+	public static void main(String[] args) 
+	{
+		
+		int b=10;
+         int a=20;
+        int c=2;
+        
+        System.out.println(a+b);
+        
+        System.out.println(a-b);
+        
+        System.out.println(a*b);
+        
+        System.out.println(a%b);
+        
+        System.out.println(c/b);
+        
+        System.out.println(c%b);
+        a++;
+        
+        System.out.println(a);
+        a--;
+        
+        System.out.println(a);
+	}
+
+}
